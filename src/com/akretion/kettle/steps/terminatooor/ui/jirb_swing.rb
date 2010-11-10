4075644ba08c8426d044669abb7862bb583fb4ad
Raphaël Valyi <rvalyi@akretion.com>
2010-11-10 03:47:53 -0200

    fixed Interactive console custom lib/rubygems auto-load of path

--- src/com/akretion/kettle/steps/terminatooor/ui/jirb_swing.rb
+++ src/com/akretion/kettle/steps/terminatooor/ui/jirb_swing.rb
@@ -2,13 +2,19 @@ require 'jruby'
 require 'irb'
 require 'irb/completion'
 
-#in case jruby-ooor is launch from the TerminatOOOR plugin directory:
-strStartScript += "\n$:.unshift File.dirname(__FILE__) + '/custom_ruby_libs/lib'";
-strStartScript += "\nENV['GEM_HOME'] = File.dirname(__FILE__) + '/custom_ruby_libs'";
-
-#in case it's launched from Kettle libext:
-strStartScript += "\n$:.unshift File.dirname(__FILE__) + '../plugins/steps/termintatooor/custom_ruby_libs/lib'";
-strStartScript += "\nENV['GEM_HOME'] = File.dirname(__FILE__) + '../plugins/steps/termintatooor/custom_ruby_libs'";
+if File.directory? File.dirname(__FILE__) + '/custom_ruby_libs' #in case jruby-ooor is launched from the TerminatOOOR plugin directory:
+	puts "AAAAAAA"
+  $:.unshift File.dirname(__FILE__) + '/custom_ruby_libs/lib'
+  ENV['GEM_HOME'] = File.dirname(__FILE__) + '/custom_ruby_libs'
+elsif File.directory? File.dirname(__FILE__) + '/../plugins/steps/termintatooor/custom_ruby_libs' #in case it's launched from Kettle libext:
+	puts "BBBBBB"
+  $:.unshift File.dirname(__FILE__) + '/../plugins/steps/termintatooor/custom_ruby_libs/lib'
+  ENV['GEM_HOME'] = File.dirname(__FILE__) + '/../plugins/steps/termintatooor/custom_ruby_libs'
+else
+	puts "CCCCCC"
+  $:.unshift File.dirname(__FILE__) + '/plugins/steps/termintatooor/custom_ruby_libs/lib'
+  ENV['GEM_HOME'] = File.dirname(__FILE__) + '/plugins/steps/termintatooor/custom_ruby_libs'
+end
 
 require 'rubygems'
 require 'ooor'
