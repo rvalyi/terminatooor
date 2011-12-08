f176fb6cd1525de858a26b5e097c85fb4eb01428
Raphaël Valyi <rvalyi@akretion.com>
2011-12-08 19:53:29 -0200

    guard against RVM magic! RVM sets GEM_HOME and GEM_PATH environment variables which can mess up with our JRuby rubygems. See coming commits

--- src/org/typeexit/kettle/plugin/steps/ruby/execmodels/SimpleExecutionModel.java
+++ src/org/typeexit/kettle/plugin/steps/ruby/execmodels/SimpleExecutionModel.java
@@ -6,6 +6,10 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+import java.lang.reflect.Field;
 
 import org.apache.commons.lang.ArrayUtils;
 import org.jruby.RubyArray;
@@ -59,6 +63,11 @@ public class SimpleExecutionModel implements ExecutionModel {
 
 		try {
 
+            HashMap envMap = new HashMap();
+            envMap.put("GEM_HOME", new String(step.getPluginDir() + Const.FILE_SEPARATOR + "gems"));
+            envMap.put("GEM_PATH", new String(step.getPluginDir() + Const.FILE_SEPARATOR + "gems"));
+            setEnv(envMap);
+
 			data.forcedHalt = false;
 
 			data.container = RubyStepFactory.createScriptingContainer(true, meta.getRubyVersion());
@@ -104,6 +113,43 @@ public class SimpleExecutionModel implements ExecutionModel {
 
 		return true;
 	}
+
+  protected static void setEnv(Map<String, String> newenv)
+  {
+    try
+      {
+          Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+          Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+          theEnvironmentField.setAccessible(true);
+          Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+          env.putAll(newenv);
+          Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+          theCaseInsensitiveEnvironmentField.setAccessible(true);
+          Map<String, String> cienv = (Map<String, String>)     theCaseInsensitiveEnvironmentField.get(null);
+          cienv.putAll(newenv);
+      }
+      catch (NoSuchFieldException e)
+      {
+        try {
+          Class[] classes = Collections.class.getDeclaredClasses();
+          Map<String, String> env = System.getenv();
+          for(Class cl : classes) {
+              if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+                  Field field = cl.getDeclaredField("m");
+                  field.setAccessible(true);
+                  Object obj = field.get(env);
+                  Map<String, String> map = (Map<String, String>) obj;
+                  map.clear();
+                  map.putAll(newenv);
+              }
+          }
+        } catch (Exception e2) {
+          e2.printStackTrace();
+        }
+      } catch (Exception e1) {
+          e1.printStackTrace();
+      } 
+  }
 
 	private void setGemHome() {
 
